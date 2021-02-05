24900d5643952e15a864a22f0796a3e3f2325306
rxf113 <1131310577@qq.com>
2021-02-05 17:47:54 +0800

    add ignore

--- src/test/java/com/rxf113/converter/SelectFieldsTest.java
+++ src/test/java/com/rxf113/converter/SelectFieldsTest.java
@@ -41,5 +41,40 @@ public class SelectFieldsTest {
         String s = selectFieldsConverter.convertSql(originSql, fieldsControls);
         System.out.println(s);
     }
-
+//public ListNode addTwoNumbers(ListNode l1,ListNode l2){
+//        // 2 4 3
+//        // 5 6 4
+//        int flagA = 1;
+//        int flagB = 1;
+//        int ca = 0;
+//        ListNode resNode = null;
+//        while (true){
+//            int cuA = 0;
+//            int cuB = 0;
+//            if(l1 == null ){
+//                flagA--;
+//            }else {
+//                cuA = l1.val;
+//                l1 = l1.next;
+//            }
+//            if(l2 == null ){
+//                flagB--;
+//            }else {
+//                cuB = l2.val;
+//                l2 = l2.next;
+//            }
+//            if(flagA == 0 && flagB == 0){
+//                break;
+//            }
+//            int tempSum = cuA + cuB + ca;
+//            int val = tempSum % 10;
+//            ca = tempSum/10;
+//            if(resNode == null){
+//                resNode = new ListNode(val);
+//            }else {
+//                createNode(val,resNode);
+//            }
+//        }
+//        return resNode;
+//    }
 }
