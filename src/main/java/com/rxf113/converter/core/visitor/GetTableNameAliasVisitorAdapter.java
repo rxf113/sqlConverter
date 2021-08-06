31a18bbbf76753316abe3cfb31abe8d1b8754b90
我是个文盲 <1131310577@qq.com>
2021-08-06 22:25:30 +0800

    style

--- src/main/java/com/rxf113/converter/core/visitor/GetTableNameAliasVisitorAdapter.java
+++ src/main/java/com/rxf113/converter/core/visitor/GetTableNameAliasVisitorAdapter.java
@@ -1,6 +1,8 @@
 package com.rxf113.converter.core.visitor;
 
+import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.statement.SQLExprTableSource;
+import com.alibaba.druid.util.JdbcConstants;
 import com.rxf113.converter.core.model.Table;
 
 import java.util.ArrayList;
@@ -26,5 +28,24 @@ public class GetTableNameAliasVisitorAdapter implements CusVisitorAdapter<SQLExp
         return true;
     }
 
+    public static void main(String[] args) {
+
+
+
+            String sql = "update t set name = 'x' where id < 100 limit 10";
+            String mysql = SQLUtils.formatMySql(sql);
+            String oracle = SQLUtils.formatOracle(sql);
+            System.out.println("mysql: " + mysql);
+            System.out.println("oracle: " + oracle);
+            String result = SQLUtils.format(sql, JdbcConstants.MYSQL);
+            System.out.println(result); // 缺省大写格式
+
+            String result_lcase = SQLUtils.format(sql
+                    , JdbcConstants.MYSQL
+                    , SQLUtils.DEFAULT_LCASE_FORMAT_OPTION);
+            System.out.println(result_lcase); // 小写格式
+
+
+    }
 
 }
